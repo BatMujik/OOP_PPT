1ab88a20948e25c806fd9b3cd98824d880ded9d4
BatMujik <79758372+BatMujik@users.noreply.github.com>
2025-12-23 12:25:43 +0400

    Add files via upload

---
+++ build/CMakeFiles/slideshow.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slideshow.dir/src/Canvas.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/Canvas.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/Color.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/Color.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/CommandParser.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/CommandParser.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/Shape.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/Shape.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/ShapeFactory.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/ShapeFactory.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/Slide.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/Slide.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/SlideShow.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/SlideShow.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/Tokenizer.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/Tokenizer.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/UndoManager.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/UndoManager.cpp.obj.d"
+  "CMakeFiles/slideshow.dir/src/main.cpp.obj"
+  "CMakeFiles/slideshow.dir/src/main.cpp.obj.d"
+  "bin/slideshow.exe"
+  "bin/slideshow.exe.manifest"
+  "bin/slideshow.pdb"
+  "libslideshow.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slideshow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
